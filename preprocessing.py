b32cf16763ba04cb5e912e9fb804bbee1a4ed52c
Lizard077 <92269042+Lizard077@users.noreply.github.com>
2025-05-09 18:06:14 -0500

    Add files via upload

---
+++ preprocessing.py
@@ -0,0 +1,55 @@
+import json
+import re
+
+def clean_review(text):
+    
+    
+    text = re.sub(r'\\u[0-9a-fA-F]{4}|\\', '', text)
+    
+    text = re.sub(r'[‘’]', "'", text)
+    text = re.sub(r'[“”]', '"', text)
+
+    text = text.lower()
+
+    text = re.sub(r'<[^>]+>', '', text)
+
+    text = re.sub(r'full\s+spoiler[-\s]?free\s+review\s*(at|@|of)?\s*["“][^"”]*["”]\s*','', text, flags=re.IGNORECASE)
+
+    text = re.sub(r"http\S+|www\S+", "", text)
+
+    text = re.sub(r'[^a-z0-9\s.,!?:\'"-]', ' ', text) # remove special characters
+    text = re.sub(r'\s+', ' ', text)
+    text = re.sub(r'\s{2,}', ' ', text)
+
+    return text
+
+def extract_rating(text):
+    match = re.search(r'rating:\s*([a-fA-F][+-]?)', text, flags=re.IGNORECASE)
+    return match.group(1).upper() if match else None
+
+def preprocess_data(raw_data):
+    cleaned_data = []
+
+    for movie in raw_data:
+        title = movie.get("title", "").strip()
+        reviews = movie.get("reviews", [])
+
+        cleaned_reviews = []
+        for review in reviews:
+            cleaned = clean_review(review)
+            if cleaned:
+                rating = extract_rating(cleaned)
+                cleaned_reviews.append({"text": cleaned, "rating": rating})
+
+        if cleaned_reviews:
+            cleaned_data.append({"title": title, "reviews": cleaned_reviews})
+    return cleaned_data
+
+with open("movies_with_reviews.json", "r", encoding="utf-8") as infile:
+    raw_data = json.load(infile)
+
+cleaned_data = preprocess_data(raw_data)
+
+with open("cleaned_movies_with_reviews.json", "w", encoding="utf-8") as outfile:
+    json.dump(cleaned_data, outfile, indent=2, ensure_ascii=False)
+    
